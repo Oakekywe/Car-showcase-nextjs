d9abd5f0bbf0a208f71acf984a805d6d50d708aa
oakekywe <oakekywe.zotefamily@gmail.com>
2023-09-01 11:35:45 +0630

    [new] car catalogue ui, manufacturer filter and install headless ui to implement filter box

--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,8 @@ export interface CustomButtonProps {
   title: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
+
+export interface SearchManufacturer {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+}
